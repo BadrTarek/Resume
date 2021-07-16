a40d6c333d07bd16e6e2692e7c09634066336958
BadrTarek <71422548+BadrTarek@users.noreply.github.com>
2021-07-16 19:51:50 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -23,10 +23,10 @@ $(document).ready(function(){
 		profile_nav("#about-me-link" , "#about-me" , e);
 	});
 
-	$("#education-link").click(function(e){
-		e.preventDefault();
-		profile_nav("#education-link" , "#education" , e);
-	});
+	// $("#education-link").click(function(e){
+	// 	e.preventDefault();
+	// 	profile_nav("#education-link" , "#education" , e);
+	// });
 
 	$("#skills-link").click(function(e){
 		e.preventDefault();
@@ -37,9 +37,9 @@ $(document).ready(function(){
 		profile_nav("#about-me-link" , "#about-me" , e);
 	});
 
-	$("#education-nav").click(function(e){
-		profile_nav("#education-link" , "#education" , e);
-	});
+	// $("#education-nav").click(function(e){
+	// 	profile_nav("#education-link" , "#education" , e);
+	// });
 
 	$("#skills-nav").click(function(e){
 		profile_nav("#skills-link" , "#skills" , e);
@@ -49,127 +49,12 @@ $(document).ready(function(){
 		profile_nav("#about-me-link" , "#about-me" , e);
 	});
 
-	$("#education-nav-footer").click(function(e){
-		profile_nav("#education-link" , "#education" , e);
-	});
+	// $("#education-nav-footer").click(function(e){
+	// 	profile_nav("#education-link" , "#education" , e);
+	// });
 
 	$("#skills-nav-footer").click(function(e){
 		profile_nav("#skills-link" , "#skills" , e);
 	});
 
-
-
-
-	/*$("#education-link").click(function(e){
-		e.preventDefault();
-		$("#about-me-link").parent().removeClass("active");
-		$("#education-link").parent().addClass("active");
-		
-
-		$("#about-me").fadeOut("slow",function(){
-			$("#education").fadeIn("slow");
-		});
-
-	});*/
-
-
-
-
-	/*
-	function aboutMeFadeIn(){
-		$("#about-me-link").parent().addClass("active");
-		$("#experience-link").parent().removeClass("active");
-
-		$("#experience").fadeOut("slow",function(){
-			$("#about-me").fadeIn("slow");
-		});
-	};
-	function experienceFadeIn(){
-		$("#experience-link").parent().addClass("active");
-		$("#about-me-link").parent().removeClass("active");
-		$("#about-me").fadeOut("slow",function(){
-			$("#experience").fadeIn("slow",function(){
-				$(".precentage-slide .slide div").each(function(){
-					$(this).css("width",$(this).attr("precentage"));
-				});
-			});
-		});
-	};
-	$(".about-nav").click(function(e){
-		if($("#about-me").css("display")=="none"){
-			aboutMeFadeIn();
-		}
-	});
-	$(".experience-nav").click(function(e){
-		if($("#experience").css("display")=="none"){
-			experienceFadeIn();
-		}
-	});
-	$("#about-me-link").click(function(e){
-		e.preventDefault();
-		aboutMeFadeIn();
-	});
-	$("#experience-link").click(function(e){
-		e.preventDefault();
-		experienceFadeIn();
-	});
-	function skillsCarousel()
-	{
-		var numberOfslides = $(".skills-container .skills-container-group").length;
-		var openSlide = 1;
-		$(window).scroll(function() {
-			if($(window).scrollTop()>660){
-				$(".skills-container .skills-container-group:nth-child("+openSlide+") .skill-card .slide div").each(function(){
-				 	$(this).css("width",$(this).attr("precentage"));
-				});
-			}
-		});
-		if($(window).scrollTop()>1000){
-			$(".skills-container .skills-container-group:nth-child("+openSlide+") .skill-card .slide div").each(function(){
-			 	$(this).css("width",$(this).attr("precentage"));
-			});
-		}
-		$(".next").click(function(e){
-			if(openSlide!=numberOfslides){
-				$(".skills-container .skills-container-group:nth-child("+openSlide+")").animate({
-					marginLeft:"-90%"
-				},{ 
-					duration: 1000, 
-					queue: false ,
-					easing: "swing" ,
-					complete: function(){
-						$(".skills-container .skills-container-group:nth-child("+openSlide+") .skill-card .slide div").each(function(){
-						 	$(this).css("width",$(this).attr("precentage"));
-						 });
-					}
-				});
-
-				openSlide++;
-			}else{
-				e.preventDefault();
-			}
-		});
-		$(".back").click(function(e){
-			if(openSlide!=1)
-			{
-				openSlide--;
-				$(".skills-container .skills-container-group:nth-child("+openSlide+")").animate({
-					marginLeft:"5%"
-				},{
-				 	duration: 1000,
-				  	queue: false ,
-				  	easing: "swing",
-				  	complete: function(){
-						$(".skills-container .skills-container-group:nth-child("+openSlide+") .skill-card .slide div").each(function(){
-						 	$(this).css("width",$(this).attr("precentage"));
-						 });
-					}
-				});
-			}else{
-				e.preventDefault();
-			}
-		});
-	}
-	skillsCarousel();
-	*/
 });
